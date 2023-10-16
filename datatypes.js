511803907c846b331265c96c44942aa5ff670dfa
Krishna Gopal <74735407+KrishnaGopalPatel@users.noreply.github.com>
2023-10-16 11:20:13 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+// primitive 
+
+// 7 types : String, number, boolean, null undefined, Symbol, Bigint
+
+const day=100 
+const fee=201.25
+
+const isloggedin=false
+const outsidetemp=null
+let useremail;
+
+const id = Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+
+const bigNumber=5546546566665855582n
+
+// Refrence (Non primitive)
+
+// Array, Objects, Functions
+
+const heros=["shaktiman", "naagraj","doga"];
+let obj=//object
+{
+    name:"hitesh",
+    age: 22,
+}
+
+
+const myfunction=function(){
+    console.log(typeof outsidetemp);
+}
